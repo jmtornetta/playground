bafb34ce21dcda5a9a8bae59d27a8a6d89267dff
Jon Tornetta <jmtornetta@gmail.com>
2018-09-15 22:42:16 -0400

    Added messages to freecodecamp exercises.

--- freeCodeCamp/record-collection.js
+++ freeCodeCamp/record-collection.js
@@ -1,1 +1,73 @@
-{"index.js":"// Setup\nvar collection = {\n    \"2548\": {\n      \"album\": \"Slippery When Wet\",\n      \"artist\": \"Bon Jovi\",\n      \"tracks\": [ \n        \"Let It Rock\", \n        \"You Give Love a Bad Name\" \n      ]\n    },\n    \"2468\": {\n      \"album\": \"1999\",\n      \"artist\": \"Prince\",\n      \"tracks\": [ \n        \"1999\", \n        \"Little Red Corvette\" \n      ]\n    },\n    \"1245\": {\n      \"artist\": \"Robert Palmer\",\n      \"tracks\": [ ]\n    },\n    \"5439\": {\n      \"album\": \"ABBA Gold\"\n    }\n};\n// Keep a copy of the collection for tests\nvar collectionCopy = JSON.parse(JSON.stringify(collection));\n\n// Only change code below this line\nfunction updateRecords(id, prop, value) {\n  if(prop != 'tracks' && value != ''){\n    collection[id][prop] = value;\n  } else if(prop =='tracks' && collection[id].hasOwnProperty(prop) == false) {\n    collection[id][prop] = [];\n    collection[id][prop].push(value);\n  } else if(prop =='tracks' && value != ''){\n    collection[id][prop].push(value);\n  } else if(value ==''){\n    delete collection[id][prop];\n  };\n  \n  \n  return collection;\n}\n\n// Alter values below to test your code\nupdateRecords(5439, \"artist\", \"ABBA\");\n"}
+/*Basic JavaScript: Record Collection
+You are given a JSON object representing a part of your musical album collection. Each album has several properties and a unique id number as its key. Not all albums have complete information.
+
+Write a function which takes an album's id (like 2548), a property prop (like "artist" or "tracks"), and a value (like "Addicted to Love") to modify the data in this collection.
+
+If prop isn't "tracks" and value isn't empty (""), update or set the value for that record album's property.
+
+Your function must always return the entire collection object.
+
+There are several rules for handling incomplete data:
+
+If prop is "tracks" but the album doesn't have a "tracks" property, create an empty array before adding the new value to the album's corresponding property.
+
+If prop is "tracks" and value isn't empty (""), push the value onto the end of the album's existing tracks array.
+
+If value is empty (""), delete the given prop property from the album.
+
+Hints
+Use bracket notation when accessing object properties with variables.
+
+Push is an array method you can read about on Mozilla Developer Network.
+
+You may refer back to Manipulating Complex Objects Introducing JavaScript Object Notation (JSON) for a refresher. */
+
+// Setup
+var collection = {
+    "2548": {
+      "album": "Slippery When Wet",
+      "artist": "Bon Jovi",
+      "tracks": [ 
+        "Let It Rock", 
+        "You Give Love a Bad Name" 
+      ]
+    },
+    "2468": {
+      "album": "1999",
+      "artist": "Prince",
+      "tracks": [ 
+        "1999", 
+        "Little Red Corvette" 
+      ]
+    },
+    "1245": {
+      "artist": "Robert Palmer",
+      "tracks": [ ]
+    },
+    "5439": {
+      "album": "ABBA Gold"
+    }
+};
+// Keep a copy of the collection for tests
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+// Only change code below this line
+function updateRecords(id, prop, value) {
+  if (prop === "tracks" && value !== "") {
+   if(collection[id][prop]) {
+    collection[id][prop].push(value);
+   }
+   else {
+    collection[id][prop]=[value];
+   }
+  } else if (value !== "") {
+    collection[id][prop] = value;
+  } else {
+    delete collection[id][prop];
+  }
+
+  return collection;
+}
+
+// Alter values below to test your code
+updateRecords(5439, "artist", "ABBA");
